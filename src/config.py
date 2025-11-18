79e4c5138aaf0b6dd0495029137e053d50899c59
thienducthinh <96965880+thienducthinh@users.noreply.github.com>
2025-11-18 01:24:22 -0500

    Update config.py

--- src/config.py
+++ src/config.py
@@ -15,7 +15,7 @@ class Settings(BaseSettings):
     DB_MAX_OVERFLOW: int = 40
     
     # Security
-    SECRET_KEY: str
+    # SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7
